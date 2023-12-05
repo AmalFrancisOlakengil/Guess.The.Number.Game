979bc43bd7074650855455c8faff2310858169ba
AmalFrancisOlakengil <143199482+AmalFrancisOlakengil@users.noreply.github.com>
2023-12-05 22:49:58 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,28 @@
+from random import randint
+run = True
+while (run):
+     Target = randint(0,100)
+     i = 1
+     while (i<=5):
+      print("You have %d attempts"%(6-i))
+      Guess = int(input("Guess my number: "))
+      if Guess == Target:
+         choice = input('Your Right\nContinue?\nyes or no:')
+         if choice == 'yes':
+            run = True
+            break
+         else:
+            run = False
+            break
+      elif Guess < Target:
+         print("Your Guess is low than Target")
+      else:
+         print("Your Guess is high than Target")
+      i += 1
+     if i == 6:
+        choice = input('Continue?\nyes or no:')
+        if choice == 'yes':
+            run = True
+           
+        else:
+            run = False
